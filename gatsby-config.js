19be3dd0f68f6b5df9ead066491ae343e8ed39fa
Giuseppe Germinaro <g.germinaro88@gmail.com>
2019-11-18 11:21:55 +0100

    Removed leftover page and changed theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Andshape`,
-    description: `AN creative agency for entrepreneurs`,
+    description: `A creative agency for entrepreneurs`,
     author: `@giuge`,
   },
   plugins: [
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1C252C`,
+        theme_color: `#1C252C`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
